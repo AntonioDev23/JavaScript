9c9bc3ba9b227227958e6d939d1c2564757855a6
ANTONIO  NETO <snetinho40@gmail.com>
2025-01-27 20:00:11 -0300

    Aula 02

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function adicionarTarefa() {
+    //let mensagem = "Tarefa adicionada com sucesso!";
+
+    let inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value
+    document.getElementById("mensagem").textContent = tarefa;    
+  }
