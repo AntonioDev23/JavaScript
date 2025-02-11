584c03822eafcc1b4b4065b083a585934418b778
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-11 18:56:48 -0300

    up

--- Rafa/script.js
+++ Rafa/script.js
@@ -1,19 +1,24 @@
 
-function adicionarTarefa() {
+
+  function adicionarTarefa() {
     let mensagem = "Tarefa adicionada com sucesso!";
   
     let inputTarefa = document.getElementById("inputTarefa");
     let tarefa = inputTarefa.value.trim(); // O `trim()` remove espaços em branco no início e no final
   
+    let mensagemElemento = document.getElementById("mensagem");
+  
     // Verificar se o campo está vazio
     if (tarefa === "") {
-      // Se estiver vazio, mostra uma mensagem ou faz nada
-      document.getElementById("mensagem").textContent = "Por favor, digite uma tarefa!";
+      // Se estiver vazio, mostra uma mensagem de erro
+      mensagemElemento.textContent = "Por favor, digite uma tarefa!";
+      mensagemElemento.style.color = "red"; // Define a cor vermelha
       return; // Impede de continuar com a função
     }
   
     // Se o campo não estiver vazio, adiciona a tarefa
-    document.getElementById("mensagem").textContent = mensagem;
+    mensagemElemento.textContent = mensagem;
+    mensagemElemento.style.color = "green"; // Define a cor verde
   
     let listaTarefas = document.getElementById("listaTarefas");
     let novaTarefa = document.createElement("li");
@@ -23,6 +28,5 @@ function adicionarTarefa() {
   
     // Limpa o campo de input
     inputTarefa.value = "";
-    
-    
   }
+
