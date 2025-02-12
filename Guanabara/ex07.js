60d8b36124668611adbfb7d1d7f496e17f4d3b2d
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-12 18:20:42 -0300

    ex7

---
+++ Guanabara/ex07.js
@@ -0,0 +1,8 @@
+
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel} km/hr`)
+if (vel > 60) { // condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!!!')
+}
+
+console.log('Sempre dirija usando cinto de segurança')
