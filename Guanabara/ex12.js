a0047397249a39276861694c7f0860ea585fe803
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-17 18:45:05 -0300

    ex12

---
+++ Guanabara/ex12.js
@@ -0,0 +1,7 @@
+let dia = 6;
+switch (dia) {
+    case 1: console.log("Segunda"); break;
+    case 2: console.log("Terça"); break;
+    case 3: console.log("Quarta"); break;
+    default: console.log("Outro dia");
+}
