0d4915503167e073cc59abcecc8c2b97dbeb71ed
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-17 18:49:10 -0300

    up

--- Guanabara/ex12.js
+++ Guanabara/ex12.js
@@ -1,7 +1,12 @@
-let dia = 6;
-switch (dia) {
+let agora = new Date()
+var diaSem = agora.getDay()
+switch (diaSem) {
+    case 0: console.log("Domingo"); break;
     case 1: console.log("Segunda"); break;
     case 2: console.log("Terça"); break;
     case 3: console.log("Quarta"); break;
-    default: console.log("Outro dia");
+    case 4: console.log("Quinta"); break;
+    case 5: console.log("Sexta"); break;
+    case 6: console.log("Sábado"); break;
+    default: console.log("Dia inválido"); break;
 }
