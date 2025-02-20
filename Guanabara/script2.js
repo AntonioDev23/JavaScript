2d5060afa64a5465cd9a34696a30ae3c6ab59f40
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-20 18:50:55 -0300

    up

--- Guanabara/script2.js
+++ Guanabara/script2.js
@@ -1,41 +1,24 @@
-function verificar () {
-   var data =  new Date()
-   var ano = data.getFullYear
-   var fano = document.getElementById('ano')
-   var res = document.querySelector('div#res')
-   if (fano.value.length == 0 || fano.value > ano) {
-    alert("Invalid")
-   } else {
-        var fsex = document.getElementsByName("sex")
-        var idade = ano - Number(fano.value)
-        var genero = ""
-        if (fsex [0] .checked) {
-            genero = "Homem"
-            if (idade >=0 && idade < 10) {
-                //criança
-            } else if ( idade < 21) {
-                //adolescente
-            } 
-            else if (idade < 60) {
-                //adulto
-            } else {
-                //idoso
-            }
-        } else if (fsex [1] .checked) {
-            genero = "Mulher"
-            if (idade >=0 && idade < 10) {
-                //criança
-            } else if (idade < 21) {
-                //adolescente
-            } 
-            else if (idade < 60) {
-                //adulto
-            } else {
-                //idoso
-            }
-            
+function verificar() {
+    var data = new Date();
+    var ano = data.getFullYear(); // Correção aqui
+    var fano = document.getElementById('ano');
+    var res = document.querySelector('div#res');
+ 
+    if (fano.value.length == 0 || Number(fano.value) > ano) { // Correção aqui
+        alert("Ano inválido! Verifique os dados e tente novamente.");
+    } else {
+        var fsex = document.getElementsByName("sex");
+        var idade = ano - Number(fano.value);
+        var genero = "";
+ 
+        if (fsex[0].checked) {
+            genero = "Homem";
+        } else if (fsex[1].checked) {
+            genero = "Mulher";
         }
-            res.style.textalign = "center"
-            res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-   }
-} 
+ 
+        res.style.textAlign = "center"; // Correção aqui
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+    }
+ }
+ 
