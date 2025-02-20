6b0480bdf011d5430d65ae6717af7516d9bee1d0
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-20 18:37:24 -0300

    up

--- Guanabara/script2.js
+++ Guanabara/script2.js
@@ -11,8 +11,28 @@ function verificar () {
         var genero = ""
         if (fsex [1] .checked) {
             genero = "Homem"
+            if (idade >= 0 && idade < 10) {
+                res.innerHTML = "Detectamos uma criança"
+            } else if  (idade < 21) {
+                res.innerHTML = "Detectamos um jovem adulto"
+            }
+            else if (idade < 50) {
+                res.innerHTML = "Detectamos um adulto"
+            } else {
+                res.innerHTML = "Detectamos um idoso"
+            }
         } else if (fsex [1] .checked) {
             genero = "Mulher"
+            if (idade >= 0 && idade < 10) {
+                res.innerHTML = "Detectamos uma criança"
+            } else if  (idade < 21) {
+                res.innerHTML = "Detectamos uma jovem adulta"
+            }
+            else if (idade < 50) {
+                res.innerHTML = "Detectamos uma adulta"
+            } else {
+                res.innerHTML = "Detectamos um idosa"
+            }
         }
             res.style.textalign = "center"
             res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
