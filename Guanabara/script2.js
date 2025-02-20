3da8dba790f7e0ceaa8fc5fafb81fddf916255dd
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-20 18:30:10 -0300

    up

--- Guanabara/script2.js
+++ Guanabara/script2.js
@@ -6,6 +6,8 @@ function verificar () {
    if (fano.ariaValueMax.length === 0 || fano.value > ano) {
     alert("Invalid")
    } else {
-    
+        var fsex = document.getElementsByName("sex")
+        var idade = ano - Number(fano.value)
+        
    }
 }
