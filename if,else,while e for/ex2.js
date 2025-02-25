6a499cf3512520a103bcc19598542ddc4dd83174
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-25 18:24:00 -0300

    upex2

--- if,else,while e for/ex2.js
+++ if,else,while e for/ex2.js
@@ -1,17 +1,12 @@
-// Pede um número ao usuário e converte para inteiro
-let numero = parseInt(prompt("Digite um número positivo:"));
+const readline = require('readline-sync');
 
-// Inicializa a variável soma para armazenar o resultado
+let numero = parseInt(readline.question("Digite um número positivo: "));
 let soma = 0;
-
-// Inicializa um contador começando em 1
 let i = 1;
 
-// Enquanto o contador for menor ou igual ao número informado
 while (i <= numero) {
-    soma += i; // Adiciona o valor de i à soma
-    i++; // Incrementa o contador
+    soma += i;
+    i++;
 }
 
-// Exibe o resultado final da soma
 console.log(`A soma de 1 até ${numero} é ${soma}.`);
