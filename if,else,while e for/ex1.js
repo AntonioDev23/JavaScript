c04d82beb24d17a43ac9be36566bd57c4e6eb1a0
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-25 18:17:51 -0300

    ex1

---
+++ if,else,while e for/ex1.js
@@ -0,0 +1,9 @@
+// Pede um número ao usuário e converte para inteiro
+let numero = parseInt(prompt("Digite um número:"));
+
+// Verifica se o número é divisível por 2 (par)
+if (numero % 2 === 0) {
+    console.log(`${numero} é par.`);
+} else { // Caso contrário, é ímpar
+    console.log(`${numero} é ímpar.`);
+}
