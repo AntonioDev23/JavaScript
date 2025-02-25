c1393096c98faa6c7287e2cd144ae7ded0659fdb
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-25 18:25:11 -0300

    upex6

--- if,else,while e for/ex6.js
+++ if,else,while e for/ex6.js
@@ -1,25 +1,22 @@
-// Pede um número ao usuário e converte para inteiro
-let numero = parseInt(prompt("Digite um número:"));
+const readline = require('readline-sync');
 
-// Inicializa a variável para verificar se o número é primo
+let numero = parseInt(readline.question("Digite um número: "));
 let ehPrimo = true;
 
-// Números menores que 2 não são primos
 if (numero < 2) {
     ehPrimo = false;
 } else {
-    // Loop de 2 até o número - 1 para verificar se ele é divisível por algum outro número
     for (let i = 2; i < numero; i++) {
-        if (numero % i === 0) { // Se for divisível, não é primo
+        if (numero % i === 0) {
             ehPrimo = false;
-            break; // Sai do loop, pois já sabemos que não é primo
+            break;
         }
     }
 }
 
-// Exibe o resultado final
 if (ehPrimo) {
     console.log(`${numero} é primo.`);
 } else {
     console.log(`${numero} não é primo.`);
 }
+
