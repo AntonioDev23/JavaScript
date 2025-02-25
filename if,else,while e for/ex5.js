5e1fd3ce284b4baf80a1296abd28727e32bf8896
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-25 18:20:20 -0300

    ex5

---
+++ if,else,while e for/ex5.js
@@ -0,0 +1,7 @@
+// Loop que começa em 10 e decrementa até 1
+for (let i = 10; i >= 1; i--) {
+    console.log(i); // Exibe o número atual
+}
+
+// Exibe a mensagem final após a contagem regressiva
+console.log("Feliz Ano Novo! 🎉");
