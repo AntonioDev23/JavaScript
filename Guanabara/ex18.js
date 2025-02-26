60ecd1515f3930bfdba5e710ae5ba7c7d90fc01e
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-26 19:33:18 -0300

    up

--- Guanabara/ex18.js
+++ Guanabara/ex18.js
@@ -2,5 +2,7 @@ let num = [5,2,8,9,3]
 num [5] = 7
 console.log(`Nosso vetor é o ${num}`)
 
-for (var i = 0; i < num.length; i++)
-    console.log(`O valor na posição ${i} é ${num[i]}`)
+
+for (let pos in num) {
+    console.log(`O valor na posição ${pos} é ${num[pos]}`)
+}
