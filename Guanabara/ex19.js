a2432ba342ea13db50563d43e9e65625658366f0
ANTONIO  NETO <snetinho40@gmail.com>
2025-02-27 18:17:18 -0300

    up

--- Guanabara/ex19.js
+++ Guanabara/ex19.js
@@ -1,17 +1,6 @@
 
-function parimpar(n) {
-    if (n%2 == 0) {
-        return 'Par'
-        } else {
-        return 'Impar'
-    
-    }
-} console.log(parimpar(223))
 
-function soma(n1=0, n2=0) {
-    return n1 + n2
-} console.log(soma(7,3 ))
-
-let v = function() {
-    
-}
+let v = function(x) {
+    return x*2
+} 
+console.log(v(5))
