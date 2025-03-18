f5fbc00c222143fc7d9babf49928ab8fb1f2031b
ANTONIO  NETO <snetinho40@gmail.com>
2025-03-18 19:26:31 -0300

    up

--- js-avançado/script.js
+++ js-avançado/script.js
@@ -4,3 +4,13 @@ console.log(minhavar)
  
 var eu = "Neto"
 console.log(eu)
+
+function saudacao() {
+    console.log("Olá, Neto!");
+}
+saudacao(); // Chamada da função
+
+const saudacao = function() {
+    console.log("Olá, Neto!");
+};
+saudacao(); // Chamada da função
